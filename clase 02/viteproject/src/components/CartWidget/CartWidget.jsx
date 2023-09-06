336f40de6a498b4ea15dcf70e44b42eb25ccb84a
Gabriel Frias <GabrielFrias@roagro.local>
2023-09-05 21:14:40 -0300

    ComponentsII

---
+++ clase 02/viteproject/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,11 @@
+import carrito from '../../assets/carrito.png'
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src={carrito} />
+        </div>
+    )
+}
+
+export default CartWidget
---
+++ clase 02/viteproject/src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,15 @@
+import './styles.css'
+
+const ItemListContainer = () => {
+
+  return (
+    <div className='container'>
+      <p>Home</p>
+      <p>Catalogo</p>
+      <p>Contacto</p>
+      <p>Ayuda</p>
+    </div>
+  );
+};
+
+export default ItemListContainer;
